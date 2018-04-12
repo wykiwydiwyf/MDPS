8cd6cca6abb383fe81e1972b9be922988b34bc99
Yifei Wang <wykiwydiwyf@gmail.com>
2018-04-13 01:30:40 +1000

    111

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<title>INFS7901-Tables</title>
-</head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>INFS7901-Tables</title>
+  </head>
 <body>
 
   <h1>INFS7901 Project: Medical Database Processing System(MEPS)</h1>
 
-    <?php 
+  <?php 
         // SETUP PHP CONNECTION
         $servername = "localhost";
         $username = "root";
@@ -28,25 +28,26 @@
   echo "where d.doc_id = pd2.doc_id and p.pat_id = pd2.pat_id and pd2.if_surge = 1<br>";
 
     ?>
-    
-  
+
+
   <table class="table thead-light table-bordered">
-        <thead>
-            <tr>
-                <th scope="col">Patient ID</th>
-                <th scope="col">Patient Name</th>
-                <th scope="col">Doctor ID</th>
-                <th scope="col">Doctor Name</th>
-                <th scope="col">Disease</th>
-                <th scope="col">Treatment</th>
-            </tr>
-        </thead>
+    <thead>
+      <tr>
+        <th scope="col">Patient ID</th>
+        <th scope="col">Patient Name</th>
+        <th scope="col">Doctor ID</th>
+        <th scope="col">Doctor Name</th>
+        <th scope="col">Disease</th>
+        <th scope="col">Treatment</th>
+      </tr>
+    </thead>
     <form action="" method="post">
       <input type="submit" name="query1" class="btn btn-primary btn-lg" value="Get Data" style="text-align:right;margin:10px" />
     </form>
-        <tbody id="queryTable1">
-          <?php
+    <tbody id="queryTable1">
+      <?php
                 // FILL TABLE WITH DATA ON CLICK
+                if(isset($_POST["submit"])) {
                     $query = "SELECT p.pat_id,p.pat_name,d.doc_id,d.doc_name,pd2.disease,pd2.treatment
 from doctor as d, pat_doc_2 as pd2 ,patient_1 as p
 where d.doc_id = pd2.doc_id and p.pat_id = pd2.pat_id and pd2.if_surge = 1
@@ -62,21 +63,22 @@ where d.doc_id = pd2.doc_id and p.pat_id = pd2.pat_id and pd2.if_surge = 1
                         echo "<td>".$rows["disease"]."</td>";
                         echo "<td>".$rows["treatment"]."</td>";
                         echo "</tr>";
+                  }
                 }
             ?>
-        </tbody>
-    </table>
-  
+    </tbody>
+  </table>
+
 
 
   <?php  
   echo "<h5>MYSQL Insertion1</h5>";
   echo "<h5>Insert New Patient Information</h5>";
     ?>
-  
-  
 
-  
+
+
+
   <form action="includes/signup.inc.php" method="POST">
     <div class="form-group">
       <label for="form1">Patient's Name</label>
@@ -114,7 +116,7 @@ where d.doc_id = pd2.doc_id and p.pat_id = pd2.pat_id and pd2.if_surge = 1
   </form>
 
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
