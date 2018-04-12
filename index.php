9e53a30af72a60c40115d69c33afa4413c86a57c
Yifei Wang <wykiwydiwyf@gmail.com>
2018-04-13 01:14:32 +1000

    111

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
     ?>
     
   
-  <table class="table table-dark">
+  <table class="table thead-light table-bordered">
         <thead>
             <tr>
                 <th scope="col">Patient ID</th>
@@ -91,10 +91,17 @@ where d.doc_id = pd2.doc_id and p.pat_id = pd2.pat_id and pd2.if_surge = 1
                                     <button type="submit" name="submit">Sign up</button>
                                   </form>
 
+  
+  <form action="includes/signup.inc.php" method="POST">
+    <div class="form-group">
+      <label for="form1">Patient's Name</label>
+      <input type="text" class="form-control" id="form1" name="pat_name" placeholder="Patient's Name">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
 
 
-
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
