c2f269153f12d20f6b815346d3672ab59448db0b
Yifei Wang <wykiwydiwyf@gmail.com>
2018-04-14 00:44:43 +1000

    111

--- includes/logged_in_as_doc.php
+++ includes/logged_in_as_doc.php
@@ -244,13 +244,13 @@ $result = mysqli_query($conn, $query);
         <option selected="">Choose...</option>
           <?php 
           while ($rows = mysqli_fetch_array($result)) {
-          echo "<option value='".$row["pat_id"]."'>'".$row["pat_id"]."'</option>";
+          echo "<option value=".$row["pat_id"].">".$row["pat_id"]."</option>";
           }
            ?>
         
       </select>
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-primary">Assign</button>
   </form>
   
   
