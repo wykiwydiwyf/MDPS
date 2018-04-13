57958190050fe8f038c2104fd45bedb3b32b721c
Yifei Wang <wykiwydiwyf@gmail.com>
2018-04-13 15:53:02 +1000

    111

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,13 +1,21 @@
 <?php 
-
+        // SETUP PHP CONNECTION
+        $servername = "localhost";
+        $username = "root";
+        $password = "d74dbdad52b2dfe8";
+        $dbname = "project_hospital";
         
-$mysqli = new mysqli("localhost", "root", "d74dbdad52b2dfe8", "project_hospital");
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
+        if ($conn->connect_error) {
+            die("<h3>Connection failed: ".$conn->connect_error."</h3>");
+        }
+    ?>
 
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
+
+
+<?php 
+
 
 $pat_name=$_POST['pat_name'];
 $age=$_POST['age'];
@@ -25,25 +33,27 @@ $dur_in_hos=$_POST['dur_in_hos'];
 $query  = "INSERT INTO patient_1(pat_id,hos_name,name,age,gender,address,visit_date) VALUES(NULL,(SELECT hos_name FROM hospital),'$pat_name','$age','$gender','$address','$visit_date')";
 $query .= "INSERT INTO patient_2(pat_id,hos_name,symptom,dur_in_hos,T) VALUES(NULL,(SELECT hos_name FROM hospital),'symptom','T','dur_in_hos')";
 
-/* execute multi query */
-if ($mysqli->multi_query($query)) {
-    do {
-        /* store first result set */
-        if ($result = $mysqli->store_result()) {
-            while ($row = $result->fetch_row()) {
-                printf("%s\n", $row[0]);
-            }
-            $result->free();
+// Execute multi query
+if (mysqli_multi_query($con,$sql))
+{
+  do
+    {
+    // Store first result set
+    if ($result=mysqli_store_result($con)) {
+      // Fetch one and one row
+      while ($row=mysqli_fetch_row($result))
+        {
+        printf("%s\n",$row[0]);
         }
-        /* print divider */
-        if ($mysqli->more_results()) {
-            printf("-----------------\n");
-        }
-    } while ($mysqli->next_result());
+      // Free result set
+      mysqli_free_result($result);
+      }
+    }
+  while (mysqli_next_result($con));
 }
 
 //let them know the person has been added. 
 echo "Data successfully inserted into the database table ... ";
 /* close connection */
-$mysqli->close();
+mysqli_close($con);
 ?>
