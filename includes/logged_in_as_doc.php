294d5a0a04b1ff8c6bc36acce09d952a35d24725
Yifei Wang <wykiwydiwyf@gmail.com>
2018-04-13 23:24:01 +1000

    111

--- includes/logged_in_as_doc.php
+++ includes/logged_in_as_doc.php
@@ -221,15 +221,7 @@ from doctor
   </table>
 
 
-
-  
-  
-  <form action="signup.inc2.php" method="POST" style="text-align:left;margin-bottom:300px,margin-top:200px,margin-left:100px;margin-right:600px" >
-    <div class="form-group">
-      <label for="form1">Patient ID</label>
-      <select class="custom-select" name="pat_id" id="form1">
-        <?php 
-        
+  <?php 
 $query1 = "SELECT pat_id
 from patient_1 as p1
 Where exists
@@ -241,29 +233,23 @@ Where exists
 where pd1.pat_id= p1.pat_id))
 ";
 $result = mysqli_query($conn, $query1);
+  
+        ?>
+  
+  <form action="signup.inc2.php" method="POST" style="text-align:left;margin-bottom:300px,margin-top:200px,margin-left:100px;margin-right:600px" >
+    <div class="form-group">
+      <label for="form1">Patient ID</label>
+      <select class="custom-select" name="pat_id" id="form1">
 while ($row = mysql_fetch_array($result))
 {
     echo "<option value='".$row['path']."'>'".$row['name']."'</option>";
 }
-        ?>
-      </select>
-    </div>
-    
-    <div class="form-group">
-      <label for="form2">Doctor ID</label>
-      <select class="custom-select" name="doc_id" id="form2">
-        <?php 
-
-
-while ($row = mysql_fetch_array($result2))
-{
-    echo "<option value='".$row['path']."'>'".$row['name']."'</option>";
-}
-?>
       </select>
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>
+  
+  
   
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
