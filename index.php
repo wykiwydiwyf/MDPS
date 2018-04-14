2c4261ffaec8a9fe36894e58025cdbea6bbdb7c7
Yifei Wang <wykiwydiwyf@gmail.com>
2018-04-14 10:30:08 +1000

    111

--- index.php
+++ index.php
@@ -27,7 +27,6 @@
   </div>
 
 
-  <img src="..." class="img-fluid" alt="Responsive image">
 
 
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
