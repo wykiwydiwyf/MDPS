cf4d36284f4111c6a3dc27f81feae7e812590cea
Yifei Wang <wykiwydiwyf@gmail.com>
2018-04-14 12:26:02 +1000

    222

--- includes/doc_insert/insert1.php
+++ includes/doc_insert/insert1.php
@@ -170,8 +170,11 @@ $date="$year-$month-$dt";
 if(isset($_POST["insert1"]) && $_POST["insert1"] != "") {
 
   $query = "INSERT INTO patient_1(pat_id,doc_id,date) VALUES($pat_id,$doc_id,$date)";
-                      $result = mysqli_query($conn, $query);
                       mysqli_query($conn, $query);
+                        //let them know the person has been added. 
+  echo mysql_errno($link) . ": " . mysql_error($link) . "\n";
+  echo "<span style='color:Green;'> PatientID $pat_id has successfully assigned with DoctorID $doc_id for diagnosis date $date ... </span>";
+  echo str_repeat('&nbsp;', 100);
               }
 
 ?>
