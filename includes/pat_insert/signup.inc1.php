6af89e4898a975ca659b8745a66680f8c7de840f
Yifei Wang <wykiwydiwyf@gmail.com>
2018-04-15 17:39:02 +1000

    .

--- includes/pat_insert/signup.inc1.php
+++ includes/pat_insert/signup.inc1.php
@@ -38,8 +38,6 @@ $visit_date="$year-$month-$dt";
 
 $sql  = "INSERT INTO patient_1(pat_id,hos_name,pat_name,age,gender,address,visit_date) VALUES(NULL,(SELECT hos_name FROM hospital),'$pat_name','$age','$gender','$address','$visit_date');";
 $sql .= "INSERT INTO patient_2(pat_id,hos_name,symptom,dur_in_hos,T) VALUES(NULL,(SELECT hos_name FROM hospital),'$symptom','$dur_in_hos','$T')";
-$sql .= "INSERT INTO pat_doc_1(pat_id) VALUES(LAST_INSERT_ID())";
-$sql .= "INSERT INTO pat_doc_2(pat_id) VALUES(LAST_INSERT_ID())";
 
 // Execute multi query
 if (mysqli_multi_query($conn,$sql))
