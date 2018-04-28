cb00635ebec0b4d12c3f9b4bdc0c5edb521a19ce
Yifei Wang <wykiwydiwyf@gmail.com>
2018-04-28 20:29:18 +1000

    1

--- includes/pat_insert/signup.inc1.php
+++ includes/pat_insert/signup.inc1.php
@@ -40,43 +40,9 @@ $sql  = "INSERT INTO patient_1(pat_id,pat_name,age,gender,address,visit_date,sym
 
 if ($T = "1")
 {
-  $sql .= "INSERT INTO inpatient(pat_id,hos_name,dur_in_hos) VALUES(LAST_INSERT_ID(),'$hos_name','$dur_in_hos')";
-  if (mysqli_multi_query($conn,$sql))
-{
-  do
-    {
-    // Store first result set
-    if ($result=mysqli_store_result($conn)) {
-      // Fetch one and one row
-      while ($row=mysqli_fetch_row($result))
-        {
-        printf("%s\n",$row[0]);
-        }
-      // Free result set
-      mysqli_free_result($result);
-      }
-    }
-  while (mysqli_next_result($conn));
-}
+  $sql .= "INSERT INTO inpatient(pat_id,hos_name,dur_in_hos) VALUES(LAST_INSERT_ID(),(SELECT hos_name FROM hospital WHERE hos_name = '$hos_name'),'$dur_in_hos')";
 }else{
-  $sql .= "INSERT INTO outpatient(pat_id,hos_name) VALUES(LAST_INSERT_ID(),'$hos_name')";
-  if (mysqli_multi_query($conn,$sql))
-{
-  do
-    {
-    // Store first result set
-    if ($result=mysqli_store_result($conn)) {
-      // Fetch one and one row
-      while ($row=mysqli_fetch_row($result))
-        {
-        printf("%s\n",$row[0]);
-        }
-      // Free result set
-      mysqli_free_result($result);
-      }
-    }
-  while (mysqli_next_result($conn));
-}
+  $sql .= "INSERT INTO outpatient(pat_id,hos_name) VALUES(LAST_INSERT_ID(),(SELECT hos_name FROM hospital WHERE hos_name = '$hos_name'))";
 }
 
 // Execute multi query
