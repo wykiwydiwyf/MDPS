a7cfce6cd4ec4ba9fb0b2119c2df3f592f705369
Yifei Wang <wykiwydiwyf@gmail.com>
2018-04-28 22:18:51 +1000

    1

--- index.php
+++ index.php
@@ -27,12 +27,16 @@
   </div>
 
 
-<?php
-header("Content-type: image/png");
-readfile("$file");
-exit;
-?>
+<img src="er_project.png" />
 
+<style>
+img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 300px;
+}
+</style>
 
 
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
