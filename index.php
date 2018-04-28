d2c639ff104049342d16528d628a048922847182
Yifei Wang <wykiwydiwyf@gmail.com>
2018-04-28 22:16:19 +1000

    1

--- index.php
+++ index.php
@@ -28,34 +28,9 @@
 
 
 <?php
-function LoadPNG($imgname)
-{
-    /* Attempt to open */
-    $im = @imagecreatefrompng($imgname);
-
-    /* See if it failed */
-    if(!$im)
-    {
-        /* Create a blank image */
-        $im  = imagecreatetruecolor(150, 30);
-        $bgc = imagecolorallocate($im, 255, 255, 255);
-        $tc  = imagecolorallocate($im, 0, 0, 0);
-
-        imagefilledrectangle($im, 0, 0, 150, 30, $bgc);
-
-        /* Output an error message */
-        imagestring($im, 1, 5, 5, 'Error loading ' . $imgname, $tc);
-    }
-
-    return $im;
-}
-
-header('Content-Type: image/png');
-
-$img = LoadPNG('er_project.image');
-
-imagepng($img);
-imagedestroy($img);
+header("Content-type: image/png");
+readfile("$file");
+exit;
 ?>
 
 
