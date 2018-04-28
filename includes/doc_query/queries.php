034ab309c21524054734ee4530d1bd3a320009e6
Yifei Wang <wykiwydiwyf@gmail.com>
2018-04-29 02:17:15 +1000

    1

--- includes/doc_query/queries.php
+++ includes/doc_query/queries.php
@@ -53,207 +53,26 @@
 
 <main class="col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content" role="main">
 
-<h2 id="query1"><div>All Surgeries<a class="anchorjs-link " href="#query1" aria-label="Anchor" data-anchorjs-icon="#" style="padding-left: 0.375em;"></a></div></h2>
-  <?php
-  echo str_repeat('&nbsp;', 10);
-  echo "<h5>MYSQL Query1 - Join Query :Information for all surgeries </h5>";
-  echo "SELECT p.pat_id,p.pat_name,d.doc_id,d.doc_name,pd2.disease,pd2.treatment<br>";
-  echo "from doctor as d, pat_doc_2 as pd2 ,patient_1 as p<br>";
-  echo "where d.doc_id = pd2.doc_id and p.pat_id = pd2.pat_id and pd2.if_surge = 1<br>";
-  echo str_repeat('&nbsp;', 5);
-    ?>
-  <form action="" method="post">
-    <input type="submit" name="query1" class="btn btn-primary btn-lg" value="Run Query1" style="text-align:right;margin:10px" />
-  </form>
-  <table class="table thead-light table-bordered" style="margin-top:100px;margin-bottom:100px;margin-left:100px;margin-right:300px">
-    <thead>
-      <tr>
-        <th scope="col">Patient ID</th>
-        <th scope="col">Patient Name</th>
-        <th scope="col">Doctor ID</th>
-        <th scope="col">Doctor Name</th>
-        <th scope="col">Disease</th>
-        <th scope="col">Treatment</th>
-      </tr>
-    </thead>
-
-    <tbody id="queryTable1">
-      <?php
-            if(isset($_POST["query1"]) && $_POST["query1"] != "") {
-
-                    $query = "SELECT p.pat_id,p.pat_name,d.doc_id,d.doc_name,pd2.disease,pd2.treatment from doctor as d, pat_doc_2 as pd2 ,patient_1 as p where d.doc_id = pd2.doc_id and p.pat_id = pd2.pat_id and pd2.if_surge = 1";
-                    $result = mysqli_query($conn, $query);
-                    
-                    while ($rows = mysqli_fetch_array($result)) {
-                        echo "<tr>";
-                        echo "<td>".$rows["pat_id"]."</td>";
-                        echo "<td>".$rows["pat_name"]."</td>";
-                        echo "<td>".$rows["doc_id"]."</td>";
-                        echo "<td>".$rows["doc_name"]."</td>";
-                        echo "<td>".$rows["disease"]."</td>";
-                        echo "<td>".$rows["treatment"]."</td>";
-                        echo "</tr>";
-                    }
-            }
-            ?>
-
-    </tbody>
-  </table>
-  <h2 id="query2"><div>Show who is/are the doctors for each patients<a class="anchorjs-link " href="#query2" aria-label="Anchor" data-anchorjs-icon="#" style="padding-left: 0.375em;"></a></div></h2>
-  <?php
-  echo str_repeat('&nbsp;', 10);
-  echo "<h5>MYSQL Query2 - Join Query :Show who is/are the doctors for each patients </h5>";
-  echo "SELECT p.pat_id,p.pat_name,d.doc_id,d.doc_name<br>
-from doctor as d, pat_doc_1 as pd1 ,patient_1 as p<br>
-where d.doc_id = pd1.doc_id and p.pat_id = pd1.pat_id<br>
-order by p.pat_id
-<br>";
-  echo str_repeat('&nbsp;', 5);
-    ?>
-  <form action="" method="post">
-    <input type="submit" name="query2" class="btn btn-primary btn-lg" value="Run Query2" style="text-align:right;margin:10px" />
-  </form>
-  <table class="table thead-light table-bordered" style="margin-top:100px;margin-bottom:100px;margin-left:100px;margin-right:300px">
-    <thead>
-      <tr>
-        <th scope="col">Patient ID</th>
-        <th scope="col">Patient Name</th>
-        <th scope="col">Doctor ID</th>
-        <th scope="col">Doctor Name</th>
-      </tr>
-    </thead>
-
-    <tbody id="queryTable2">
-      <?php
-            if(isset($_POST["query2"]) && $_POST["query2"] != "") {
-
-$query = "SELECT p.pat_id,p.pat_name,d.doc_id,d.doc_name
-from doctor as d, pat_doc_1 as pd1 ,patient_1 as p
-where d.doc_id = pd1.doc_id and p.pat_id = pd1.pat_id 
-order by p.pat_id";
-                    $result = mysqli_query($conn, $query);
-                    
-                    while ($rows = mysqli_fetch_array($result)) {
-                        echo "<tr>";
-                        echo "<td>".$rows["pat_id"]."</td>";
-                        echo "<td>".$rows["pat_name"]."</td>";
-                        echo "<td>".$rows["doc_id"]."</td>";
-                        echo "<td>".$rows["doc_name"]."</td>";
-                        echo "</tr>";
-                    }
-            }
-            ?>
-
-    </tbody>
-  </table>
-
-<h2 id="query3"><div>Show all inpatient’s information who hasn’t been signed with a doctor<a class="anchorjs-link " href="#query3" aria-label="Anchor" data-anchorjs-icon="#" style="padding-left: 0.375em;"></a></div></h2>
-  <?php
-  echo str_repeat('&nbsp;', 10);
-  echo "<h5>MYSQL Query3 - Division Query :Show all inpatient’s information who hasn’t been signed with a doctor </h5>";
-  echo "SELECT *<br>
-from patient_1 as p1<br>
-Where exists<br>
-	(select doc_id<br>
-    from doctor as d<br>
-    where not exists<br>
-    (select pd1.doc_id<br>
-    from pat_doc_1 as pd1<br>
-where pd1.pat_id= p1.pat_id))
-<br>";
-  echo str_repeat('&nbsp;', 5);
-    ?>
-  <form action="" method="post">
-    <input type="submit" name="query3" class="btn btn-primary btn-lg" value="Run Query3" style="text-align:right;margin:10px" />
-  </form>
-  <table class="table thead-light table-bordered" style="margin-top:100px;margin-bottom:100px;margin-left:100px;margin-right:300px">
-    <thead>
-      <tr>
-        <th scope="col">Patient ID</th>
-        <th scope="col">Patient Name</th>
-        <th scope="col">Visit Date</th>
-        <th scope="col">AGE</th>
-        <th scope="col">Gender</th>
-        <th scope="col">Address</th>
-      </tr>
-    </thead>
-
-    <tbody id="queryTable3">
-      <?php
-            if(isset($_POST["query3"]) && $_POST["query3"] != "") {
-
-$query = "SELECT *
-from patient_1 as p1
-Where exists
-	(select doc_id
-    from doctor as d
-    where not exists
-    (select pd1.doc_id
-    from pat_doc_1 as pd1
-where pd1.pat_id= p1.pat_id))
-";
-                    $result = mysqli_query($conn, $query);
-                    
-                    while ($rows = mysqli_fetch_array($result)) {
-                        echo "<tr>";
-                        echo "<td>".$rows["pat_id"]."</td>";
-                        echo "<td>".$rows["pat_name"]."</td>";
-                        echo "<td>".$rows["visit_date"]."</td>";
-                        echo "<td>".$rows["age"]."</td>";
-                        echo "<td>".$rows["gender"]."</td>";
-                        echo "<td>".$rows["address"]."</td>";
-                        echo "</tr>";
-                    }
-            }
-            ?>
-
-    </tbody>
-  </table>
-
-
-<h2 id="query4"><div>Show all doctor information<a class="anchorjs-link " href="#query4" aria-label="Anchor" data-anchorjs-icon="#" style="padding-left: 0.375em;"></a></div></h2>
-  <?php
-  echo str_repeat('&nbsp;', 10);
-  echo "<h5>MYSQL Query4 - Simple Query :Show all doctor information </h5>";
-  echo "SELECT *<br>
-from doctor
-<br>";
-  echo str_repeat('&nbsp;', 5);
-    ?>
-  <form action="" method="post">
-    <input type="submit" name="query4" class="btn btn-primary btn-lg" value="Run Query4" style="text-align:right;margin:10px" />
-  </form>
-  <table class="table thead-light table-bordered" style="margin-top:100px;margin-bottom:100px;margin-left:100px;margin-right:300px">
-    <thead>
-      <tr>
-        <th scope="col">Doctor ID</th>
-        <th scope="col">Doctor Name</th>
-        <th scope="col">AGE</th>
-      </tr>
-    </thead>
-
-    <tbody id="queryTable4">
-      <?php
-            if(isset($_POST["query4"]) && $_POST["query4"] != "") {
-
-$query = "SELECT *
-from doctor
-";
-                    $result = mysqli_query($conn, $query);
-                    
-                    while ($rows = mysqli_fetch_array($result)) {
-                        echo "<tr>";
-                        echo "<td>".$rows["doc_id"]."</td>";
-                        echo "<td>".$rows["doc_name"]."</td>";
-                        echo "<td>".$rows["age"]."</td>";
-                        echo "</tr>";
-                    }
-            }
-            ?>
-
-    </tbody>
-  </table>
-
+<div class="card text-center">
+  <div class="card-header">
+    <ul class="nav nav-tabs card-header-tabs">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
 
 
   
