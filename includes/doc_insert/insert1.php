8eb123abc433292231664c5718ee5d130f301371
Yifei Wang <wykiwydiwyf@gmail.com>
2018-04-28 23:11:16 +1000

    1

--- includes/doc_insert/insert1.php
+++ includes/doc_insert/insert1.php
@@ -87,68 +87,11 @@ $result = mysqli_query($conn, $query);
     <label for="table1">Diagnosis Date</label>
     <table border="0" cellspacing="0" id="table1">
 
-<tr><td  align=left  >   
+    <tr><td  align=left  >   
 
-<input type="date" id="date" name="date">
+    <input type="date" id="date" name="date">
 
-<select name=month value=''>Select Month</option>
-<option value='01'>January</option>
-<option value='02'>February</option>
-<option value='03'>March</option>
-<option value='04'>April</option>
-<option value='05'>May</option>
-<option value='06'>June</option>
-<option value='07'>July</option>
-<option value='08'>August</option>
-<option value='09'>September</option>
-<option value='10'>October</option>
-<option value='11'>November</option>
-<option value='12'>December</option>
-</select>
-
-
-
-</td><td  align=left  >   
-
-Date<select name=dt >
-
-<option value='01'>01</option>
-<option value='02'>02</option>
-<option value='03'>03</option>
-<option value='04'>04</option>
-<option value='05'>05</option>
-<option value='06'>06</option>
-<option value='07'>07</option>
-<option value='08'>08</option>
-<option value='09'>09</option>
-<option value='10'>10</option>
-<option value='11'>11</option>
-<option value='12'>12</option>
-<option value='13'>13</option>
-<option value='14'>14</option>
-<option value='15'>15</option>
-<option value='16'>16</option>
-<option value='17'>17</option>
-<option value='18'>18</option>
-<option value='19'>19</option>
-<option value='20'>20</option>
-<option value='21'>21</option>
-<option value='22'>22</option>
-<option value='23'>23</option>
-<option value='24'>24</option>
-<option value='25'>25</option>
-<option value='26'>26</option>
-<option value='27'>27</option>
-<option value='28'>28</option>
-<option value='29'>29</option>
-<option value='30'>30</option>
-<option value='31'>31</option>
-</select>
-
-</td><td  align=left  >   
-Year(yyyy)<input type=text name=year size=4 value=2005>
-
-</table>
+    </table>
 
 </div>
 <div class="form-group">
@@ -176,13 +119,9 @@ Year(yyyy)<input type=text name=year size=4 value=2005>
 $doc_id=$_POST['doc_id'];
 $pat_id=$_POST['pat_id'];
 $todo=$_POST['todo'];
-$month=$_POST['month'];
-$dt=$_POST['dt'];
-$year=$_POST['year'];
 $if_surge=$_POST['if_surge'];
 $disease=$_POST['disease'];
 $treatment=$_POST['treatment'];
-$date1="$year-$month-$dt";
 $date=$_POST['date'];
 
 
