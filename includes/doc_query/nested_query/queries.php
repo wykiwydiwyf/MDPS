1bf49a75f23d821c343884e2b08ca02790574077
Yifei Wang <wykiwydiwyf@gmail.com>
2018-04-29 15:15:31 +1000

    1

--- includes/doc_query/nested_query/queries.php
+++ includes/doc_query/nested_query/queries.php
@@ -84,16 +84,16 @@
     <div class="btn-toolbar mb-3" role="toolbar" style="padding-left: 0.375em;" aria-label="Toolbar with button groups">
     <div class="input-group">
   </div>
-  <div class="btn-group mr-5 ml-10" role="group"  aria-label="First group">
-    <button type="button" class="btn btn-secondary" onclick="location.href='queries1.php';"/>Min</button>
-    <button type="button" class="btn btn-secondary" onclick="location.href='queries1.php';"/>Max</button>
-    <button type="button" class="btn btn-secondary" onclick="location.href='queries1.php';"/>Ave</button>
+  <div class="btn-group mr-5 ml-auto" role="group"  aria-label="First group">
+    <button type="button" class="btn btn-success" onclick="location.href='queries1.php';"/>Min</button>
+    <button type="button" class="btn btn-success" onclick="location.href='queries1.php';"/>Max</button>
+    <button type="button" class="btn btn-success" onclick="location.href='queries1.php';"/>Ave</button>
   </div>
   <div class="input-group">
     <div class="input-group-prepend ml-5" >
       <div class="input-group-text" id="btnGroupAddon">Or</div>
       <div class="btn-group " role="group" aria-label="First group">
-    <button type="button" class="btn btn-secondary" onclick="location.href='queries1.php';"/>Count</button>
+    <button type="button" class="btn btn-success" onclick="location.href='queries1.php';"/>Count</button>
   </div>
           <input type="text" class="form-control" placeholder="Input age here" name="age" aria-label="Input age" aria-describedby="btnGroupAddon">
     </div>
